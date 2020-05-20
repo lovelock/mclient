3546533cc5ea23a1abc6b3616d5de1fd9d322cbb
Frost Wong <happyhackerwqc@foxmail.com>
2020-05-20 17:31:36 +0800

    disable cas

--- cmd/get.go
+++ cmd/get.go
@@ -28,6 +28,7 @@ var getCmd = &cobra.Command{
 	Short: "get value of ${key} from memcached server ${host}:${port}",
 	Run: func(cmd *cobra.Command, args []string) {
 		mc := memcache.New(fmt.Sprintf("%s:%d", host, port))
+        mc.DisableCAS = true
 
 		value, err := mc.Get(key)
 		if err != nil {
